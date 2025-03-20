3eb956565e325e89dcdc125cd0707d1e3bb37c90
vijayasekaran05 <23csvijayasekaran@acascbe.edu.in>
2025-03-20 17:47:13 +0530

    Create truffle-config.js

---
+++ Land registry/truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.0", // Specify the version here
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    },
+  },
+};
